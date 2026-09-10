9e5aada4cb2291592ff4dc5e8878791f7e12d6f6
mumblecrustt <mumblecrustt@gmail.com>
2026-09-09 20:23:41 -0400

    Add unmodified ExamplePlugin as a control test

---
+++ ExamplePlugin/ExamplePlugin.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <ToolboxUIPlugin.h>
+
+class ExamplePlugin : public ToolboxPlugin {
+public:
+    ExamplePlugin() = default;
+    ~ExamplePlugin() override = default;
+
+    const char* Name() const override { return "Example Plugin"; }
+
+    [[nodiscard]] bool HasSettings() const override { return true; }
+    void DrawSettings() override;
+    void LoadSettings(const wchar_t* folder) override;
+    void SaveSettings(const wchar_t* folder) override;
+    void Initialize(ImGuiContext* ctx, ImGuiAllocFns allocator_fns, HMODULE toolbox_dll) override;
+    void SignalTerminate() override;
+    bool CanTerminate() override;
+    // Draw user interface. Will be called every frame if the element is visible
+    void Draw(IDirect3DDevice9* pDevice) override;
+};
